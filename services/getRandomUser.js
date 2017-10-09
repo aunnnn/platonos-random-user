cc4743bdf7563ac9f6f057d2a11bd4f47012541a
aunnnn <aun.wirawit@gmail.com>
2017-10-10 01:34:44 +0700

    Support exclude user ids

--- services/getRandomUser.js
+++ services/getRandomUser.js
@@ -1,12 +1,22 @@
 
 const User = require('../src/User')
-const { send } = require('micro')
+const { send, json } = require('micro')
 
 const dateLastNDays = (n) => {
   return new Date(Date.now() - n*864e5)
 }
 const getRandomUser = async (req, res) => {
   const forUserId = req.params.forUserId
+  
+  let excluded = [];
+  if (req.query.config) {
+    //query not empty
+    const configJSON = JSON.parse(req.query.config)
+    const excludedArray = configJSON.excluded
+    if (excludedArray instanceof Array) {
+      excluded = excludedArray
+    }
+  }
 
   const last2Days = dateLastNDays(2)
 
@@ -15,11 +25,12 @@ const getRandomUser = async (req, res) => {
       $gte: last2Days,
     },
     gc_id: {
-      $ne: forUserId,
+      $nin: [forUserId, ...excluded],
     }
   }).sort({ lastInvitedAt: 1 })
   send(res, 200, {
     user: usersByLastInvitedAt,
+    excluded,
   })
 }
 
